b92b4edabdc1f7972117d2ea007a58df11e530e3
robin <robinvanuden@gmail.com>
2023-12-17 17:48:07 +0100

    Improved character assets loading speed.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # To-Bits ChangeLog
 
+## 0.5
+
+The Item update. More coming soon!
+
 ## 0.4
 
 The Map update. More coming soon!
@@ -20,3 +24,7 @@ Character personalization/randomness update
 - [ ] Changed the favicon to the players character instead of the default knight.
 - [x] Optimized colouring for the character tint.
 - [ ] Improved the image generation
+
+### Fixed
+
+- [ ] Character image loading at first load.
--- app/src/controller/ImageController.ts
+++ app/src/controller/ImageController.ts
@@ -3,6 +3,9 @@ import sharp from "sharp"
 import path from "path"
 import PlayerRepository from "../repository/PlayerRepository"
 
+let imgCharacterLeft: sharp.Sharp | undefined = undefined
+let imgCharacterRight: sharp.Sharp | undefined = undefined
+
 function hslToRgb(h: number, s: number, l: number) {
   s /= 100
   l /= 100
@@ -13,19 +16,36 @@ function hslToRgb(h: number, s: number, l: number) {
   return {r: 255 * f(0), g: 255 * f(8), b: 255 * f(4)}
 }
 
+const loadCharacterTint = async (hue: number, left: boolean): Promise<sharp.Sharp> => {
+  return sharp(path.resolve(__dirname, "../assets/character.tint.png"))
+    .flop(left)
+    .modulate({lightness: -30})
+    .tint(hslToRgb(hue, 80, 46))
+}
+
+const loadCharacterAsset = async (left: boolean): Promise<sharp.Sharp> => {
+  console.log("load character asset", "left:", left)
+  return sharp(path.resolve(__dirname, "../assets/character.png")).flop(left)
+}
+
+const loadCharacter = async (left: boolean): Promise<sharp.Sharp> => {
+  let img = (left ? imgCharacterLeft : imgCharacterRight) || await loadCharacterAsset(left)
+  if (!imgCharacterLeft && left) imgCharacterLeft = img
+  if (!imgCharacterRight && !left) imgCharacterRight = img
+  return img
+}
+
 export default function (players: PlayerRepository) {
+
   const image_router = Router()
 
   image_router.get("/image/character/:uuid.:direction.png", async (req, res) => {
     const uuid = req.params.uuid || ""
     const player = players.getBySocketUuid(uuid)
-    const heu = player?.hue || 0
+    const hue = player?.hue || 0
     const left = (req.params?.direction || "r").toLowerCase() === "l"
-    const tint = sharp(path.resolve(__dirname, "../assets/character.tint.png"))
-      .flop(left)
-      .modulate({lightness: -30})
-      .tint(hslToRgb(heu, 80, 46))
-    const img = sharp(path.resolve(__dirname, "../assets/character.png")).flop(left)
+    const img = await loadCharacter(left)
+    const tint = await loadCharacterTint(hue, left)
     let char = img.composite([{input: await tint.toBuffer()}])
     res.contentType("image/png")
     res.end(await char.toBuffer(), "utf-8")
